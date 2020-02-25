aa7afe052d7af025722f0b400993c37bab9ba7fd
Tim DuBois <tim@neophilus.net>
2020-02-25 11:22:24 +0100

    Full implementation

--- src/scheffer.jl
+++ src/scheffer.jl
@@ -27,29 +27,50 @@ function scheffer!(du,u,p,t)
     du[2] = dP = ip + ce*prmax*FR*P*(V/(V + H₂)) - mp*P - cp*P^2
 end
 
-function run_solver(;
-             nutr = 5.7, #nutrient level
-             u0 = [25.0, 2.6], #Initial populations
+function run_ss_solver(;
+             nutr = 0.5, #nutrient level
+             u0 = [40.0, 2.6], #Initial populations
              p = [ib, r, nutr, H₁, H₂, H₃, H₄, cb, prmax, ip, ce, mp, cp, K])
     prob = SteadyStateProblem(scheffer!,u0,p);
     solve(prob)
 end
 
-x = Array{Float64}(undef,1);
-y = Array{Float64}(undef,1);
-
-for pike in range(0, stop=20, length=20)
-    for bream in range(0, stop=120, length=20)
-        sol = run_solver(u0 = [bream, pike]);
-        push!(x, sol[1]);
-        push!(y, sol[2]);
+equilibria = Set(); 
+for i in range(0.0, stop=100.0, length=10)
+    for j in range(0.0, stop=10.0, length=10)
+        for k in [0.5, 1.5, 2.5];
+            eq = run_ss_solver(u0=[i,j], nutr=k);
+            push!(equilibria, (eq[1], eq[2]));
+        end
     end
 end
 
-#sol = run_solver()
+function dB(B, P)
+    FR = B^2/(B^2 + H₄^2); # fuctional response of pike
 
-#plot(sol)
-#gui()
+    ib + r*(nutr/(nutr + H₁))*B - cb*B^2 - prmax*FR*P
+end
 
-# du = sol(0.0:365,Val{1})
-# findmin(abs.(du), dims=1)
+function dP(B, P)
+    V = K*(H₃^2/(H₃^2 + B^2)); # % of lake covered vetetation
+    FR = B^2/(B^2 + H₄^2); # fuctional response of pike
+
+    ip + ce*prmax*FR*P*(V/(V + H₂)) - mp*P - cp*P^2
+end
+
+bream = range(0.0, stop=100.0, length=100)
+pike = range(0.0, stop=10.0, length=100)
+
+nutr = 2.5;
+contour(bream, pike, dB, levels=[0], color=:blue, cbar=false, legend=true, label="bream'=0, nutr=2.5");
+nutr = 1.5;
+contour!(bream, pike, dB, levels=[0], color=:red, cbar=false, label="bream'=0, nutr=1.5");
+nutr = 0.5;
+contour!(bream, pike, dB, levels=[0], color=:green, cbar=false, label="bream'=0, nutr=0.5");
+contour!(bream, pike, dP, levels=[0], color=:black, cbar=false, label="pike'=0");
+
+eq = collect(equilibria);
+scatter!(first.(eq), last.(eq), label="equilibria");
+
+gui()
+
